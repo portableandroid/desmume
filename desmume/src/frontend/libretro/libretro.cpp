9e91e69165fc91aa77ec2afcb405e0acc40f486f
Brandon Wright <bearoso@gmail.com>
2018-03-15 18:09:39 -0500

    Increase screen gap max back to 100 for non-hybrids.

--- desmume/src/frontend/libretro/libretro.cpp
+++ desmume/src/frontend/libretro/libretro.cpp
@@ -107,16 +107,19 @@ unsigned GPU_LR_FRAMEBUFFER_NATIVE_WIDTH  = 256;
 unsigned GPU_LR_FRAMEBUFFER_NATIVE_HEIGHT = 192;
 unsigned scale = 1;
 
-#define NDS_MAX_SCREEN_GAP               64
+int current_layout = LAYOUT_TOP_BOTTOM;
+
+#define NDS_MAX_SCREEN_GAP               100
 
 static inline int gap_size()
 {
-    if (nds_screen_gap > NDS_MAX_SCREEN_GAP)
-       return 64;
+    int max_gap = (current_layout == LAYOUT_HYBRID_BOTTOM_ONLY || current_layout == LAYOUT_HYBRID_TOP_ONLY) ? 64 : NDS_MAX_SCREEN_GAP;
+
+    if (nds_screen_gap > max_gap)
+       return max_gap;
+
     return nds_screen_gap;
 }
-
-int current_layout = LAYOUT_TOP_BOTTOM;
 
 struct LayoutData
 {
@@ -1358,7 +1361,7 @@ void retro_set_environment(retro_environment_t cb)
       { "desmume_gfx_texture_scaling", "Texture Scaling (xBrz); 1|2|4" },
       { "desmume_gfx_texture_deposterize", "Texture Deposterization; disabled|enabled" },
       { "desmume_screens_layout", "Screen Layout; top/bottom|bottom/top|left/right|right/left|top only|bottom only|quick switch|hybrid/top|hybrid/bottom" },
-      { "desmume_screens_gap", "Screen Gap; 0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60|61|62|63|64" },
+      { "desmume_screens_gap", "Screen Gap; 0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60|61|62|63|64|65|66|67|68|69|70|71|72|73|74|75|76|77|78|79|80|81|82|83|84|85|86|87|88|89|90|91|92|93|94|95|96|97|98|99|100" },
       { "desmume_hybrid_layout_scale", "Hybrid Layout: Scale (restart); 1|3"},
       { "desmume_hybrid_showboth_screens", "Hybrid Layout: Show Both Screens; enabled|disabled"},
       { "desmume_hybrid_cursor_always_smallscreen", "Hybrid Layout: Cursor Always on Small Screen; enabled|disabled"},
