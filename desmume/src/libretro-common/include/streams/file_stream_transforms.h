b950edc58b40971226bfa73ec9f6002e2fd4cfd7
Alberto Fustinoni <claudiuslollarius@gmail.com>
2018-07-11 11:18:47 +0900

    Logic

--- desmume/src/libretro-common/include/streams/file_stream_transforms.h
+++ desmume/src/libretro-common/include/streams/file_stream_transforms.h
@@ -53,6 +53,7 @@ RETRO_BEGIN_DECLS
 #define fgetc rfgetc
 #define fwrite rfwrite
 #define fputc rfputc
+#define fflush rfflush
 #define fprintf rfprintf
 #define ferror rferror
 #define feof rfeof
@@ -76,6 +77,8 @@ size_t rfwrite(void const* buffer,
    size_t elementSize, size_t elementCount, RFILE* stream);
 
 int rfputc(int character, RFILE * stream);
+
+int rfflush(RFILE * stream);
 
 int rfprintf(RFILE * stream, const char * format, ...);
 
